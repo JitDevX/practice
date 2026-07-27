d0e35a4eda13625a0e31d3094a34432f964e5687
Jitendra Yadav <168562307+JitDevX@users.noreply.github.com>
2026-07-27 21:35:39 +0530

    Clean up blank lines in array.js
    Removed unnecessary blank lines in array.js

--- pages/array.js
+++ pages/array.js
@@ -8,10 +8,6 @@ const initialPersons = [
   { id: 6, name: "Karan", age: 24, city: "Jaipur" },
 ];
 
-
-
-
-
 export default function Array() {
   const [persons, setPersons] = useState(initialPersons);
   const [search, setSearch] = useState("");
